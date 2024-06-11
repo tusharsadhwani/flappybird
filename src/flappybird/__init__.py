d49408dd5892e68e09c94c7bf3f6eeab522986cb
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2024-06-12 00:55:55 +0530

    Scale sprites to window size, and add gravity

--- src/flappybird/__init__.py
+++ src/flappybird/__init__.py
@@ -3,23 +3,41 @@
 from __future__ import annotations
 
 import os
+from time import sleep
 import pygame
 from pygame._sdl2 import Window, Texture, Renderer
 
 
 ASSERTS_DIR = os.path.join(os.path.dirname(__file__), "assets")
 
+WINDOW_SIZE = pygame.display.set_mode().get_size()
+
 
 def load_image(filename: str) -> pygame.Surface:
     return pygame.image.load(os.path.join(ASSERTS_DIR, filename))
 
 
-def create_sprite(filename: str, *, scale: float = 1.0) -> Window:
-    image = load_image(filename)
-    width = image.get_width()
-    height = image.get_height()
+def vw(percent: int) -> float:
+    """Scale a percent of window width to the pixel position."""
+    width, _ = WINDOW_SIZE
+    return width * percent / 100
 
-    window = Window(filename, size=(width * scale, height * scale), always_on_top=True)
+
+def vh(percent: int) -> float:
+    """Scale a percent of window height to the pixel position."""
+    _, height = WINDOW_SIZE
+    return height * percent / 100
+
+
+def create_sprite(filename: str) -> Window:
+    image = load_image(filename)
+
+    # Make the sprite sizes constant with the window height
+    scale = 0.2
+    width = vh(image.get_width() * scale)
+    height = vh(image.get_height() * scale)
+
+    window = Window(filename, size=(width, height), always_on_top=True)
     renderer = Renderer(window)
     image_texture = Texture.from_surface(renderer, image)
 
@@ -30,25 +48,29 @@ def create_sprite(filename: str, *, scale: float = 1.0) -> Window:
     return window
 
 
-def jump(bird: Window) -> None:
-    x, y = bird.position
-    bird.position = (x, max(0, y - 20))
-
-
 def main() -> None:
-    bird = create_sprite("bird.png", scale=2)
-    pipe = create_sprite("pipe.png", scale=2)
+    bird = create_sprite("bird.png")
+    pipe = create_sprite("pipe.png")
 
     _, y = bird.position
-    bird.position = (100, y)
+    bird.position = (vw(7), y)
     bird.focus()
 
-    running = True
-    while running:
+    bird_speed = 0
+    while True:
+        # gravity
+        bird_speed += vh(0.15)
+
+        x, y = bird.position
+        bird.position = (x, y + bird_speed)
         for event in pygame.event.get():
-            # if event.type == pygame.QUIT:
-            #     running = False
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
-                    jump(bird)
-                    print(bird.position)
+                    bird_speed = vh(-2.5)
+
+        _, y = bird.position
+        if y >= vh(100):
+            break
+
+        # smooth 30 fps
+        sleep(0.03)
